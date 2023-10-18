cd4371a6a1f139f037bc48142202663a891f687a
Nataliya Statova <natasha_pupkina@mail.ru>
2023-10-18 11:21:36 +0300

    задача выполнена

---
+++ src/main/java/Cat.java
@@ -0,0 +1,19 @@
+import org.springframework.stereotype.Component;
+
+@Component("cat")
+public class Cat {
+    private String message;
+
+    public Cat() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+}
